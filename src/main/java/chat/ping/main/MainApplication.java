6c7fd79d8c60fc5bc155e8a7fd84e3989d74a6f0
Ali Rahbar <alirahbar2005@gmail.com>
2024-11-05 15:31:51 -0500

    Initialized the development Branch

--- src/main/java/chat/ping/main/MainApplication.java
+++ src/main/java/chat/ping/main/MainApplication.java
@@ -3,6 +3,7 @@ package chat.ping.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Starting point of the application
 @SpringBootApplication
 public class MainApplication {
 
