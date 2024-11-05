ab05b9715e4743e1c76255e7b67c8f25cd805b4c
Ali Rahbar <alirahbar2005@gmail.com>
2024-11-05 15:28:00 -0500

    Added Test Comment

--- src/test/java/chat/ping/main/MainApplicationTests.java
+++ src/test/java/chat/ping/main/MainApplicationTests.java
@@ -3,6 +3,8 @@ package chat.ping.main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Tests will be here
+
 @SpringBootTest
 class MainApplicationTests {
 
