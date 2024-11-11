e3fbfb9721e5716191dbce80645dd6cf86ff487f
Ali Rahbar <alirahbar2005@gmail.com>
2024-11-11 14:38:24 -0500

    Fixed errors

--- src/main/java/chat/ping/main/domain/models/Message.java
+++ src/main/java/chat/ping/main/domain/models/Message.java
@@ -1,16 +1,16 @@
-package chat.ping.main.domain.models;
-
-public abstract class Message {
-
-    String threadID;
-    String senderID;
-    Object content;
-
-    public Message(Object content, Thread thread, User sender) {
-        this.content = content;
-        this.threadID = thread.threadID;
-        this.senderID = sender.userID;
-    }
-
-
-}
+//package chat.ping.main.domain.models;
+//
+//public abstract class Message {
+//
+//    String threadID;
+//    String senderID;
+//    Object content;
+//
+//    public Message(Object content, Thread thread, User sender) {
+//        this.content = content;
+//        this.threadID = thread.threadID;
+//        this.senderID = sender.getID;
+//    }
+//
+//
+//}
