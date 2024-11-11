87049669a18a6e4b50ddca9a516d6c16a1c40e5c
Ali Rahbar <alirahbar2005@gmail.com>
2024-11-11 14:18:05 -0500

    Added UserRepository Values

--- src/main/java/chat/ping/main/domain/repositories/UserRepository.java
+++ src/main/java/chat/ping/main/domain/repositories/UserRepository.java
@@ -1,9 +1,20 @@
 package chat.ping.main.domain.repositories;
 
-
+import chat.ping.main.domain.models.User;
+import jakarta.annotation.Nullable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public class UserRepository
+public interface UserRepository extends JpaRepository<User, Long>
 {
+
+    // Method to find a user by username
+    Optional<User> findByUsername(String username);
+
+    // Method to check if a user with a given username exists
+    boolean existsByUsername(String username);
+
 }
