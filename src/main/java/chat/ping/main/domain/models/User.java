fad9f66fc1cfbb839c018a4fd075b3a79b675fd1
Ali Rahbar <alirahbar2005@gmail.com>
2024-11-13 16:04:44 -0500

    Added user object

--- src/main/java/chat/ping/main/domain/models/User.java
+++ src/main/java/chat/ping/main/domain/models/User.java
@@ -1,86 +1,38 @@
-//package chat.ping.main.domain.models;
-//
-//import jakarta.persistence.*;
-//import java.util.Objects;
-//
-//
-//@Entity
-//@Table(name="users")
-//public class User
-//{
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column(nullable = false, unique = true)
-//    private String username;
-//
-//    @Column(nullable = false)
-//    private String passwordHash;
-//
-//    // Default constructor (required by JPA)
-//    public User()
-//    {}
-//
-//    // Constructor with fields
-//    public User(String username, String passwordHash)
-//    {
-//        this.username = username;
-//        this.passwordHash = passwordHash;
-//    }
-//
-//    // Getters and Setters
-//    public Long getId()
-//    {
-//        return id;
-//    }
-//
-//    public String getUsername()
-//    {
-//        return username;
-//    }
-//
-//    public void setUsername(String username)
-//    {
-//        this.username = username;
-//    }
-//
-//    public String getPasswordHash()
-//    {
-//        return passwordHash;
-//    }
-//
-//    public void setPasswordHash(String passwordHash)
-//    {
-//        this.passwordHash = passwordHash;
-//    }
-//
-//    // Override equals and hashCode for entity consistency
-//    @Override
-//    public boolean equals(Object o)
-//    {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        User user = (User) o;
-//        return Objects.equals(id, user.id) &&
-//                Objects.equals(username, user.username);
-//    }
-//
-//    @Override
-//    public int hashCode()
-//    {
-//        return Objects.hash(id, username);
-//    }
-//
-//    @Override
-//    public String toString()
-//    {
-//        return "User{" +
-//                "id=" + id +
-//                ", username='" + username + '\'' +
-//                '}';
-//    }
-//}
+package chat.ping.main.domain.models;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+
+@Entity(name="User")
+public class User
+{
+    @Id
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = SEQUENCE, generator = "user_sequence")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    // Default constructor (required by JPA)
+    public User()
+    {}
+
+    // Constructor with fields
+    public User(String username, String passwordHash)
+    {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+
+}
 ////public class User {
 ////
 ////    String userID;
